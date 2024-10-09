36c965bc34280ae133ec858a5993c0a08ee09211
Fran Martinez <pmg.webdev@gmail.com>
2024-10-09 16:40:51 +0200

    Refactor About component animations

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -8,7 +8,6 @@ const About = () => {
         initial={{ opacity: 0, y: -20 }}
         whileInView={{ opacity: 1, y: 0 }}
         transition={{ duration: 0.5 }}
-        viewport={{ once: true }}
         className="mt 20 text-center text-4xl font-semibold"
       >
         About
@@ -17,17 +16,15 @@ const About = () => {
         initial={{ opacity: 0, x: -100 }}
         whileInView={{ opacity: 1, x: 0 }}
         transition={{ duration: 1 }}
-        viewport={{ once: true }}
-        className="p-4 text-6xl uppercase lg:text-[8rem]"
+        className="p-4 text-5xl uppercase lg:text-[6rem] leading-tight tracking-wide"
       >
         {ABOUT.text1}
       </motion.h3>
       <motion.p
-        initial={{ opacity: 0, x: 100 }}
-        whileInView={{ opacity: 1, x: 0 }}
-        transition={{ duration: 1 }}
-        viewport={{ once: true }}
-        className="mt-24 pl-4 text-lg leading-lose"
+        initial={{ opacity: 0, y: 100 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        transition={{ duration: 1.5 }}
+        className="mt-10 lg:mt-24 px-4 lg:px-40 text-lg leading-lose"
       >
         {ABOUT.text2}
       </motion.p>
